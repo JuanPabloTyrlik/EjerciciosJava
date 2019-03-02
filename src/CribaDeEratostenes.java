157f3dd9ccaaacb8b675ec94af4a6c6674796f5e
JuanPabloTyrlik <juan.pablo.tyrlik@gmail.com>
2019-03-02 19:49:21 -0300

    CribaDeEratostenes added

---
+++ src/CribaDeEratostenes.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class CribaDeEratostenes {
+
+    public static LinkedList <Integer> primeNumbers (int n){
+       LinkedList <Integer> list = new LinkedList<>();
+       for (int i=2; i<=n; i++) {
+           list.add(i);
+       }
+       int j = 0;
+       int num;
+       while (j < list.size())  {
+           for (int i=2; (list.get(j)*i) <= n; i++) {
+               num = list.get(j) * i;
+               if (num > n) {break;}
+               if (list.contains(num)) {
+                   list.remove(list.indexOf(num));
+               }
+           }
+           j++;
+       }
+       return list;
+    }
+}
